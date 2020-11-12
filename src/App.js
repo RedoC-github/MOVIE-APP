ed16842dfdb19eb0b7dcca2a10a2c7c43e363e2d
RedoC-github <coderlovemedia@gmail.com>
2020-11-12 22:23:25 +0900

    Clean Code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from "react";
 
 class App extends React.Component {
-  state = {
-    count: 0
-  };
-
   render() {
     return (
-      <div>
-        <h1>COUNTER</h1>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.add}>+</button>
-        <button onClick={this.sub}>-</button>
-      </div>
+      <div></div>
     );
   }
-
-  add = () => {
-    this.setState(current => ({count: current.count + 1}));
-  };
-  sub = () => {
-    this.setState(current => ({count: current.count - 1}));  
-  };
 }
 
 export default App;
